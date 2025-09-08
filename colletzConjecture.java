9e3d79ff0ee2e66e650e2d2f5ff35f603b6a09bc
amritaa01Maurya <mauryaamrita835@gmail.com>
2025-09-08 22:01:40 +0530

    cf ques

--- colletzConjecture.java
+++ colletzConjecture.java
@@ -8,7 +8,7 @@ public class colletzConjecture {
             int k = sc.nextInt();
             int x = sc.nextInt();
             while(k-- > 0){
-                if((x - 1)%3==0){
+                if((x - 1)%3==0 && ((x-1)/3)%2 != 0){
                     x = (x-1)/3;
                 }else{
                     x = 2 * x;
