cde91d5d85715f48f38b0275754efa59f4f6edb6
amritaa01Maurya <mauryaamrita835@gmail.com>
2025-09-25 18:36:38 +0530

    cf ques

---
+++ petya_str.java
@@ -0,0 +1,11 @@
+package dailyquestions;
+
+import java.util.*;
+public class petya_str {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(Math.floor((n*m)/2));
+    }
+}
