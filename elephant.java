47c9d6a0423559f0fcfa0cdba16e7755c65a6ad6
amritaa01Maurya <mauryaamrita835@gmail.com>
2025-09-27 02:10:09 +0530

    cf ques

---
+++ elephant.java
@@ -0,0 +1,13 @@
+package dailyquestions;
+
+import java.util.*;
+public class elephant {
+    // https://codeforces.com/problemset/problem/617/A
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int step = (n + 4)/5;
+        
+        System.out.println(step);
+    }
+}
